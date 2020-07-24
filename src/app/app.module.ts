3f266660fa15e0e2d28badf22ab6bdfc6766749e
Victor Maliuchyk <v.maliuchyk@gmail.com>
2020-07-24 20:03:06 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
+import { BigPictureComponent } from './big-picture/big-picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubHeaderComponent,
+    BigPictureComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/big-picture/big-picture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-picture',
+  templateUrl: './big-picture.component.html',
+  styleUrls: ['./big-picture.component.scss']
+})
+export class BigPictureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
